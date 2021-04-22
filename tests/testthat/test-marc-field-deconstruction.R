208f5f2fe8c2f830f1de9823c26345d60050c630
Tony Fischetti <tony.fischetti@gmail.com>
2021-04-22 10:42:11 -0400

    wrote tests for marc deconstr..

---
+++ tests/testthat/test-marc-field-deconstruction.R
@@ -0,0 +1,21 @@
+context("checking functions in marc-field-deconstruction.R")
+
+
+data("books_serials_etc_sample")
+
+# marc_leader_get_info
+test_that("marc_leader_get_info() works", {
+  expect_equal(books_serials_etc_sample[, marc_leader_get_info(marc_leader)][,record_type],
+               books_serials_etc_sample[,record_type])
+  expect_equal(books_serials_etc_sample[, marc_leader_get_info(marc_leader)][,bib_level],
+               books_serials_etc_sample[,bib_level])
+})
+
+# marc_008_get_info
+test_that("marc_008_get_info() works", {
+  expect_equal(books_serials_etc_sample[, marc_008_get_info(marc_008)][,pub_date],
+               books_serials_etc_sample[,pubyear2])
+  expect_equal(books_serials_etc_sample[, marc_008_get_info(marc_008)][,lang_code],
+               books_serials_etc_sample[,language])
+})
+
